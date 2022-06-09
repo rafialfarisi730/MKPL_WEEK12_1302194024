727a0fc0ac1e4b8f4bed33b395be764ff4c63d43
rafialfarisi730 <rafialfarisi@student.telkomuniversity.ac.id>
2022-06-09 12:31:09 +0700

    commit

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,17 +5,13 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
-		
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.increment();
-		
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.decrement();
-		
 		System.out.println("Current count:" + counter.getCount());
-		
 	}
 
 }
