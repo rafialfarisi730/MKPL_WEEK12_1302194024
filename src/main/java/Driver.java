ddf584b46aa16f20e99ac2fd4287a7101eb94486
rafialfarisi730 <rafialfarisi@student.telkomuniversity.ac.id>
2022-06-09 12:40:46 +0700

    commit

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -13,7 +13,6 @@ public class Driver {
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.decrement();
-		
 		System.out.println("Current count:" + counter.getCount());
 		
 	}
